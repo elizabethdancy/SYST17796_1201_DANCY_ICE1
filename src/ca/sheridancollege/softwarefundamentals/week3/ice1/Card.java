9392f4c29b0268e6cbae7cc1c6c2bc888baeb855
kaur8826 <kaur8826@sheridancollege.ca>
2020-01-24 13:43:50 -0500

    my new code

--- src/ca/sheridancollege/softwarefundamentals/week3/ice1/Card.java
+++ src/ca/sheridancollege/softwarefundamentals/week3/ice1/Card.java
@@ -8,7 +8,8 @@ package ca.sheridancollege.softwarefundamentals.week3.ice1;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye
- * @modifiedBy
+ * @modifier = Parneet Kaur
+ * Student Number= 991589127
  */
 public class Card {
 
--- src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
+++ src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
@@ -5,28 +5,60 @@
  */
 package ca.sheridancollege.softwarefundamentals.week3.ice1;
 
+import java.util.Random;
+import java.util.Scanner;
+
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Parneet Kaur
+ * Student Number= 991589127
  */
 public class CardTrick {
+   
     
     public static void main(String[] args)
     {
+         Scanner sc = new Scanner (System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            Random r = new Random();
+            int num = r.nextInt(14);
+            c.setValue(num);
+             magicHand[i]=c;
             //c.setValue(insert call to random number generator here)
+            int num1 = r.nextInt(4);            
+            c.setSuit(Card.SUITS[num1]); 
+            magicHand[i]=c;
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        System.out.println("please enter the value of your card");
+        int answer = sc.nextInt();
+         System.out.println("please enter the suit of your card");
+        String answer1 = sc.nextLine();
+        Card diamondFive = new Card();
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+      for (int i=0; i<magicHand.length; i++)
+      {
+         if ((magicHand[i].getValue()==answer)&&(magicHand[i].getSuit().equals(answer1)))
+                 {
+                    System.out.println("Wow! You won");
+                 }
+         else
+         {
+             System.out.println("Sorry, You lost it.");
+             
+         }
+      }
     }
-    
 }
+        
+      
+    
+

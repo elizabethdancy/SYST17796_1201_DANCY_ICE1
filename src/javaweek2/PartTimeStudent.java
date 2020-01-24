e48c40e47c768bae2b514115283b31fc3ea8d8ac
burgess30043 <burgess30043@yahoo.com.tw>
2020-01-24 11:44:15 -0500

    new student to new branch

---
+++ src/javaweek2/PartTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaweek2;
+
+/**
+ *
+ * @author burge
+ */
+public class PartTimeStudent extends Student{
+    private int hourOfStudy;
+    
+    /**
+     *
+     * @param name
+     * @param studentID
+     * @param programName
+     */
+
+    
+    public PartTimeStudent(String name, String studentID, String programName){
+        super(name, studentID);
+        
+    }
+    
+}

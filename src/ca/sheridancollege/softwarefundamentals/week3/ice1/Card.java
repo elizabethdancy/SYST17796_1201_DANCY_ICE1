8b55a9a785121065a207a2297db582a9ba075bca
Devinz555 <zhangdiw@sheridanc.on.ca>
2020-01-25 03:30:23 +0800

    add a field for program1

--- src/ca/sheridancollege/softwarefundamentals/week3/ice1/Card.java
+++ src/ca/sheridancollege/softwarefundamentals/week3/ice1/Card.java
@@ -2,13 +2,13 @@ package ca.sheridancollege.softwarefundamentals.week3.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye
- * @modifiedBy
+ * @modifiedBy Diwen Zhang  StudentID:991579291
  */
 public class Card {
 
@@ -22,28 +22,32 @@ public class Card {
     public String getSuit() {
         return suit;
     }
-
     /**
      * @param suit the suit to set
      */
     public void setSuit(String suit) {
-        this.suit = suit;
+        if(suit.equalsIgnoreCase("Hearts")||suit.equalsIgnoreCase("Diamonds")||suit.equalsIgnoreCase("Spades")
+                ||suit.equalsIgnoreCase("Clubs"))
+             {this.suit = suit;}
+        else{
+            throw new RuntimeException("Error: the suit is invalid.");
+        }
     }
-
     /**
      * @return the value
      */
     public int getValue() {
         return value;
     }
-
     /**
      * @param value the value to set
      */
     public void setValue(int value) {
-        this.value = value;
+        if(value>0 && value<14){
+            this.value = value;
+        }
+        else{
+            throw new RuntimeException("Error: the value is invalid.");
+        }
     }
-   
-   
-    
 }
--- src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
+++ src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
@@ -5,28 +5,56 @@
  */
 package ca.sheridancollege.softwarefundamentals.week3.ice1;
 
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifiedBy Diwen Zhang  StudentID:991579291
  */
 public class CardTrick {
-    
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(1 + Math.random()*13));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c; 
         }
-        
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-    }
-    
+        Scanner scan = new Scanner(System.in);
+       do{
+           int value;
+           String suit;
+           Card userCard = new Card();
+           do{   
+            try{
+                System.out.println("Now pick a card from the deck for you ");
+                System.out.println("Please enter the card value: ");
+                userCard.setValue(scan.nextInt());
+                System.out.println("Please enter the card suit: ");
+                userCard.setSuit(scan.next());;
+                break;
+            }catch(RuntimeException e){
+                System.out.println("Sorry, the input is invalid.");
+            }
+           }while(true);
+            int count=0;
+            for(int i =0;i<7;i++){
+                if(userCard.getSuit().equalsIgnoreCase("magicHand[i].getSuit()") &&
+                   userCard.getValue()== magicHand[i].getValue()){
+                     count++;
+                }
+            }
+        System.out.println(count>0? "You win" :"You lose" ); 
+        System.out.println("Do you wanna try again? [Y/N]" ); 
+      }while("YES".toLowerCase().contains(scan.next()));
+   }   
 }

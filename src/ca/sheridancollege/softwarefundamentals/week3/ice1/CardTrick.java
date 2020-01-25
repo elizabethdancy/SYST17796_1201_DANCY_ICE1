9552bddc7ac6d71568ac47672f019f119040e3cc
Krzysztof Garbos <45836773+KrzysztofG13@users.noreply.github.com>
2020-01-24 23:20:51 -0500

    Add files via upload

--- src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
+++ src/ca/sheridancollege/softwarefundamentals/week3/ice1/CardTrick.java
@@ -5,28 +5,47 @@
  */
 package ca.sheridancollege.softwarefundamentals.week3.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifiedBy: Krzysztof Garbos
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
+        for (int i=0; i<magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(1);
+            magicHand[i] = c;
+            c.setSuit("Clubs");
+            magicHand[i] = c;
         }
+        Card luckyCard = new Card ();
+        luckyCard.setValue(3);
+        luckyCard.setSuit("Diamonds");
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+//        System.out.println("What card number do you want?");
+//        int cardNumber = reader.nextInt();
+//        
+//        System.out.println("What is the suit of the card?");
+//        String cardSuit = reader.nextLine();
+//        reader.nextLine();
+        
+        for (Card c : magicHand) {
+            if (luckyCard.getValue() == c.getValue() && c.getSuit().equals(luckyCard.getSuit())){
+                System.out.println("The cards match!");
+            } else {
+                System.out.println("The cards don't match!");
+                break;
+            }
+        } 
     }
-    
 }
